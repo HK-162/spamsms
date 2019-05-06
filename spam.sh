9aed11a20ec29d28c00a34414a3d4817e4a2ca52
HK-162 <37222586+HK-162@users.noreply.github.com>
2019-05-06 22:48:42 +0700

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,15 @@
+clear
+toilet -f slant -F gay "CodaShop"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mBullyHat"
+echo
+echo
+echo "\033[33;1mCOPY LISENSI INI:" "\033[36;1mJDJ5JDEwJC9YMWRvNC5jcmNOSUw2dGdUaUt2d08ucWEvWURWSFNCTXI3U21wdkdsR1FMcVBSTW1oNUZH"
+sleep 1
+echo "\033[32;1mtunggu 5 Detik"
+sleep 5
+clear
+cd $HOME/spamsms
+php virus.php
